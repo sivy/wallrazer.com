d3c116c9bdb784f4974664fef7e3d10a3ecb4ade
Steve Ivy <sivy@sixapart.com>
2011-03-01 09:57:12 -0700

    site updates

--- fabfile.py
+++ fabfile.py
@@ -4,15 +4,28 @@ from fabric.api import *
 
 env.hosts = ['monkinetic@monkinetic.com']
 
+from pystatsd import Client
+
+def record(millis):
+    c = Client('rayners.org',8125)
+    print "recording...\n"
+    c.increment('wallrazer.deploys')
+    c.timing('wallrazer.deploys.time',millis)
+
 def generate():
-	local('jekyll')
+    local('jekyll')
 
 def pack():
-	generate()
-	local('tar czf ~/tmp/wr_site.tgz _site')
+    generate()
+    local('tar czf ~/tmp/wr_site.tgz _site')
 
 def deploy():
-	pack()
-	put ('~/tmp/wr_site.tgz','tmp')
-	with cd('wallrazer.com'):
-		run ('tar xzf ~/tmp/wr_site.tgz')
+    from datetime import datetime
+    start = datetime.now()
+    pack()
+    put ('~/tmp/wr_site.tgz','tmp')
+    with cd('wallrazer.com'):
+        run ('tar xzf ~/tmp/wr_site.tgz')
+    end = datetime.now()
+    dur = (end-start).microseconds/1000
+    record(dur)
