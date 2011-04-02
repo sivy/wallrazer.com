6aed73aa8e79598a49ae2e5dd4af7a26e4331daf
Steve Ivy <steve@wallrazer.com>
2011-04-02 13:47:24 -0700

    add logging

--- post_update.sh
+++ post_update.sh
@@ -1,5 +1,10 @@
 # post_update script for actions to be taken during a deploy
 
 # generate the site
+echo "building site..."
 /var/lib/gems/1.8/bin/jekyll
+
+
+# copy to web dir
+echo "copying to /var/www/"
 cp -R _site/* /var/www/
