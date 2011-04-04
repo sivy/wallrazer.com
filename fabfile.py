ac8edd119a52bc6bf8a5b130aa4ad398fb90d615
Steve Ivy <steve@wallrazer.com>
2011-04-04 09:16:56 -0700

    add css changes, start on blog layout

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,7 @@ env.hosts = ['monkinetic@monkinetic.com']
 from pystatsd import Client
 
 def record(millis):
-    c = Client('rayners.org',8125)
+    c = Client('hammer.wallrazer.com',8125)
     print "recording...\n"
     c.increment('wallrazer.deploys')
     c.timing('wallrazer.deploys.time',millis)
@@ -20,11 +20,13 @@ def pack():
     local('tar czf ~/tmp/wr_site.tgz _site')
 
 def deploy():
+    env.hosts = ['sivy@hammer.wallrazer.com']
+    
     from datetime import datetime
     start = datetime.now()
     pack()
     put ('~/tmp/wr_site.tgz','tmp')
-    with cd('wallrazer.com'):
+    with cd('/var/www/'):
         run ('tar xzf ~/tmp/wr_site.tgz')
     end = datetime.now()
     dur = (end-start).microseconds/1000
